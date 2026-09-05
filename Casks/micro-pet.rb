86c6386484b10cd81633930b15a47f0f7cc5b6e4
liyuankui <liyuankui@gmail.com>
2026-09-05 12:41:48 +0800

    Update: micro-pet 0.1.0 asset with dedicated bundle id (com.liyuankui.micro-pet)

--- Casks/micro-pet.rb
+++ Casks/micro-pet.rb
@@ -1,6 +1,6 @@
 cask "micro-pet" do
   version "0.1.0"
-  sha256 "9f3eb54f8de2ca5aa895902d6384d43a52e2e10a8b270bf910f15d8a3b9e79c2"
+  sha256 "736eec3c2943335caf83569e6c315498b6497f0e185e484e83c14d6ac6e99b1a"
 
   url "https://github.com/liyuankui/workoutpet/releases/download/v0.1.0/micro-pet-0.1.0-macos-arm64.zip"
   name "micro-pet"
